a3182320334302abaa4553a85323eb0d91fcb63b
MaratSaidov <msaidov1@yandex.ru>
2020-10-03 16:27:18 +0300

    decrease AVG_NUM

--- tree_transformer/runs/run_code_nstack_nmt_infer.sh
+++ tree_transformer/runs/run_code_nstack_nmt_infer.sh
@@ -59,7 +59,7 @@ export nobar=1
 export LR="${LR:-0.00005}"
 export DROPOUT="${DROPOUT:-0.3}"
 export WDECAY="${WDECAY:-0.0001}"
-export AVG_NUM=10
+export AVG_NUM=5
 export LENPEN=1
 
 export ARCH=${arch}
